e9b19a2c785f0eb3370f9f67c189b194b38d31f1
devjam <admin@devjam.app>
2023-06-01 09:52:18 -0600

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
+////process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
 export const config = {
     matcher: "/((?!api|static|.*\\..*|_next).*)",
   };
@@ -24,7 +24,7 @@ export async function middleware(req) {
                 { path: '/' }
             )
         }
-        
+          
         return response
     }
 
